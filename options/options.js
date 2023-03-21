be869c41e454a09712133b292ff22c1f485726c3
Crowdbotics <team@crowdbotics.com>
2023-03-21 21:39:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "morning-wildflower-39489",
+  url: "https://morning-wildflower-39489.botics.co",
+  api: "https://morning-wildflower-39489.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
